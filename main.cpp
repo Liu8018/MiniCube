550ef267815496c092d0b632b1a3da89fa2230b0
Liu <liuyiguang8018@gmail.com>
2020-03-16 12:22:09 +0800

    ...

--- main.cpp
+++ main.cpp
@@ -7,31 +7,45 @@
 
 int main()
 {
-    MiniCubeVisualizer vizer;
+    MiniCubeVisualizer vizer_srcCube;
+    MiniCubeVisualizer vizer_dstCube;
     
-    cv::Mat image(500,1000,CV_8UC3);
-    image = cv::Scalar(255,255,255);
-    cv::Mat image_b = image.colRange(0,500);
-    cv::Mat image_f = image.colRange(500,1000);
+    cv::Mat srcImage(500,1000,CV_8UC3);
+    srcImage = cv::Scalar(255,255,255);
+    cv::Mat srcImage_b = srcImage.colRange(0,500);
+    cv::Mat srcImage_f = srcImage.colRange(500,1000);
+    vizer_srcCube.getImage(0).copyTo(srcImage_b);
+    vizer_srcCube.getImage(1).copyTo(srcImage_f);
+    cv::imshow("init state",srcImage);
     
-    vizer.getImage(0).copyTo(image_b);
-    vizer.getImage(1).copyTo(image_f);
-    cv::imshow("MiniCube",image);
-    
-    std::vector<int> initCube = vizer.getCurrentState();
+    cv::Mat dstImage(500,1000,CV_8UC3);
+    dstImage = cv::Scalar(255,255,255);
+    cv::Mat dstImage_b = dstImage.colRange(0,500);
+    cv::Mat dstImage_f = dstImage.colRange(500,1000);
+    vizer_dstCube.getImage(0).copyTo(dstImage_b);
+    vizer_dstCube.getImage(1).copyTo(dstImage_f);
+    cv::imshow("target state",dstImage);
     
     while(1){
         char key = cv::waitKey();
-        if(key == '1') vizer.vizFrontL();
-        else if(key == '3') vizer.vizFrontR();
-        else if(key == '4') vizer.vizRightL();
-        else if(key == '6') vizer.vizRightR();
-        else if(key == '9') vizer.vizTopL();
-        else if(key == '7') vizer.vizTopR();
-        else if(key == 's') {
-            std::vector<int> cube = vizer.getCurrentState();
+        if(key == '1') vizer_srcCube.vizFrontL();
+        else if(key == '3') vizer_srcCube.vizFrontR();
+        else if(key == '4') vizer_srcCube.vizRightL();
+        else if(key == '6') vizer_srcCube.vizRightR();
+        else if(key == '9') vizer_srcCube.vizTopL();
+        else if(key == '7') vizer_srcCube.vizTopR();
+        else if(key == 'a') vizer_dstCube.vizFrontL();
+        else if(key == 's') vizer_dstCube.vizFrontR();
+        else if(key == 'd') vizer_dstCube.vizRightL();
+        else if(key == 'e') vizer_dstCube.vizRightR();
+        else if(key == 'w') vizer_dstCube.vizTopL();
+        else if(key == 'q') vizer_dstCube.vizTopR();
+        else if(key == ' ') {
+            std::vector<int> srcCube = vizer_srcCube.getCurrentState();
+            std::vector<int> dstCube = vizer_dstCube.getCurrentState();
+            
             MiniCubeSolver solver;
-            bool isSolved = solver.depthFirstSolve_recursion(cube,initCube,13);
+            bool isSolved = solver.depthFirstSolve_recursion(srcCube,dstCube,13);
             
             if(isSolved){
                 std::cout<<"success"<<std::endl;
@@ -55,9 +69,13 @@ int main()
         }
         else if(key == 27) break;
         
-        vizer.getImage(0).copyTo(image_b);
-        vizer.getImage(1).copyTo(image_f);
-        cv::imshow("MiniCube",image);
+        vizer_srcCube.getImage(0).copyTo(srcImage_b);
+        vizer_srcCube.getImage(1).copyTo(srcImage_f);
+        cv::imshow("init state",srcImage);
+        
+        vizer_dstCube.getImage(0).copyTo(dstImage_b);
+        vizer_dstCube.getImage(1).copyTo(dstImage_f);
+        cv::imshow("target state",dstImage);
     }
     
     return 0;
